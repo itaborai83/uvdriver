50473a80cd0a03a237ec6d36db09df83bfd765c0
Daniel Lemos Itaboraí <itaborai83@yahoo.com.br>
2020-06-28 04:07:24 +0100

    ui WIP

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,27 +1,18 @@
 <template>
 	<div id="app">
-        <Header/>
-		<NavBar v-bind:selected="selected" @sel-nav="handleNav"/>
-		<Contents v-bind:selected="selected"/>
+       Hello World 
 	</div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import NavBar from './components/NavBar.vue'
-import Contents from './components/Contents.vue'
 
 export default {
   name: 'App',
   data: function(){
     return {
-        selected: 'home'
     }
   },
   components: {
-	Header,
-    NavBar,
-	Contents
   },
   methods: {
     handleNav: function(selectedItem) { this.selected = selectedItem; }
@@ -29,13 +20,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  display: flex;
-  align-items: stretch;
-  flex-wrap: wrap;
-  align-content: stretch;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+ font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
